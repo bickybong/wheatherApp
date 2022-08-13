ce523f730070952f336a1aa9239b97ec7b78000c
bicky <bickybong@gmail.com>
2022-08-13 15:19:14 +0800

    webapp baby

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+    app.post("/", function(req, res){
+        console.log(req.body.cityName);
+        const query = req.body.cityName;
+    const apiKey = "e159d85d8b31386b68940ec528b92557";
+    const units = "metric";
+    const url =`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${units}&appid=${apiKey}&lang=en`
+    https.get(url, function(response){
+        console.log(response.statusCode);
+        response.on("data", function(data){
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const description = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imgURL = `http://openweathermap.org/img/wn/${icon}@2x.png`
+            res.write(`<h1>The temperature in ${query} is ${temp} degrees Celcius</h1>`);
+            res.write(`<p>The weather is ${description}</p>`);
+            res.write(`<img src="${imgURL}">`)
+            res.send();
+        })
+    })
+    })
+
+    
+})
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000.");
+})
